e46ccd95bf0d4b47cb2b488e7b56b8e76d90b83d
Underrun2016 <ecampanha@hotmail.com>
2024-08-16 07:48:56 -0300

    Atualizar

--- scp_atualizar_framework.ps1
+++ scp_atualizar_framework.ps1
@@ -2,8 +2,8 @@
 # Cabeçalho
 #----------------------------------------------------------------------------------------------
 Write-Host "╔" -NoNewline -ForegroundColor Cyan
-write-host ("═" * 120) -NoNewline -ForegroundColor Cyan
-write-host "╗" -ForegroundColor Cyan  
+Write-Host ("═" * 120) -NoNewline -ForegroundColor Cyan
+Write-Host "╗" -ForegroundColor Cyan  
 
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Atualizar") -NoNewline
@@ -21,8 +21,8 @@ Write-Host ("{0,-86} " -f $MyInvocation.MyCommand.Path) -NoNewline -ForegroundCo
 Write-Host "║" -ForegroundColor Cyan
 
 Write-Host "╠" -NoNewline -ForegroundColor Cyan
-write-host ("═" * 120) -NoNewline -ForegroundColor Cyan
-write-host "╣" -ForegroundColor Cyan
+Write-Host ("═" * 120) -NoNewline -ForegroundColor Cyan
+Write-Host "╣" -ForegroundColor Cyan
 #----------------------------------------------------------------------------------------------
 
 # Iniciar Ações
@@ -40,8 +40,8 @@ Write-Host ("{0,-86} " -f $updatesDirectory) -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
 
 Write-Host "╠" -NoNewline -ForegroundColor Cyan
-write-host ("═" * 120) -NoNewline -ForegroundColor Cyan
-write-host "╣" -ForegroundColor Cyan
+Write-Host ("═" * 120) -NoNewline -ForegroundColor Cyan
+Write-Host "╣" -ForegroundColor Cyan
 
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Tarefa") -NoNewline -ForegroundColor Cyan
@@ -49,85 +49,173 @@ Write-Host ("{0,-86} " -f "Arquivo") -NoNewline -ForegroundColor Cyan
 Write-Host "║" -ForegroundColor Cyan
 
 Write-Host "║" -NoNewline -ForegroundColor Cyan
-write-host ("═" * 120) -NoNewline -ForegroundColor Gray
+Write-Host ("═" * 120) -NoNewline -ForegroundColor Gray
 Write-Host "║" -ForegroundColor Cyan
 
+# Framework 3.5
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 3.5") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"dotnetfx35.exe" -ArgumentList "/quiet" -Wait
 
+$netFrameworkPath = Join-Path $updatesDirectory "dotnetfx35.exe"
+
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 3.5 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Framework 4.5
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 4.5") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Net-Framework 4.5.exe" -ArgumentList "/quiet" -Wait
 
+$netFrameworkPath = Join-Path $updatesDirectory "Net-Framework 4.5.exe"
+
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 4.5 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Framework 4.6
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 4.6") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Net-Framework 4.6.exe" -ArgumentList "/quiet" -Wait
 
+$netFrameworkPath = Join-Path $updatesDirectory "Net-Framework 4.6.exe"
+
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 4.6 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Framework 4.6.1
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 4.6.1") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Net-Framework 4.6.1.exe" -ArgumentList "/quiet" -Wait
 
+$netFrameworkPath = Join-Path $updatesDirectory "Net-Framework 4.6.1.exe"
+
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 4.6.1 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Framework 4.7.1
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 4.7.1") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Net-Framework 4.7.1.exe" -ArgumentList "/quiet" -Wait
 
+$netFrameworkPath = Join-Path $updatesDirectory "Net-Framework 4.7.1.exe"
+
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 4.7.1 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Framework 4.7.2
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 4.7.2") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Net-Framework 4.7.2.exe" -ArgumentList "/quiet" -Wait
 
+$netFrameworkPath = Join-Path $updatesDirectory "Net-Framework 4.7.2.exe"
+
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 4.7.2 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+Write-Host "╚" -NoNewline -ForegroundColor Cyan
+Write-Host ("═" * 120) -NoNewline -ForegroundColor Cyan
+Write-Host "╝" -ForegroundColor Cyan
+
+# Framework 4.7.3
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 4.7.3") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Net-Framework 4.7.3.exe" -ArgumentList "/quiet" -Wait
 
+$netFrameworkPath = Join-Path $updatesDirectory "Net-Framework 4.7.3.exe"
+
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 4.7.3 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Framework 4.8
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 4.8") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Net-Framework 4.8.exe" -ArgumentList "/quiet" -Wait
 
+$netFrameworkPath = Join-Path $updatesDirectory "Net-Framework 4.8.exe"
+
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 4.8 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Framework 4.8.1
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
 Write-Host ("{0,-86} " -f "NET Framework 4.8.1") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Net-Framework 4.8.1.exe" -ArgumentList "/quiet" -Wait
-#----------------------------------------------------------------------------------------------
 
-# Aplicando alterações
-#----------------------------------------------------------------------------------------------
-# Aplicar alterações
-rundll32.exe user32.dll, UpdatePerUserSystemParameters
+$netFrameworkPath = Join-Path $updatesDirectory "Net-Framework 4.8.1.exe"
 
-# Verificar se o processo explorer está em execução
-$explorerProcess = Get-Process -Name explorer -ErrorAction SilentlyContinue
-
-if ($explorerProcess) {
-    Write-Host "║" -NoNewline -ForegroundColor Cyan
-    Write-Host ("{0,-30} : " -f " Reiniciando Processo") -NoNewline
-    Write-Host ("{0,-86} " -f "Windows Explorer") -NoNewline -ForegroundColor Cyan
-    Write-Host "║" -ForegroundColor Cyan
-    Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
-    Start-Process explorer -WindowStyle Hidden
+if (Test-Path $netFrameworkPath) {
+    Start-Process -FilePath $netFrameworkPath -ArgumentList "/quiet" -Wait
 } else {
     Write-Host "║" -NoNewline -ForegroundColor Cyan
-    Write-Host ("{0,-30} : " -f " Iniciando Processo") -NoNewline
-    Write-Host ("{0,-86} " -f "Windows Explorer") -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "NET Framework 4.8.1 não encontrado no diretório.") -NoNewline -ForegroundColor Red
     Write-Host "║" -ForegroundColor Cyan
-    Start-Process explorer -WindowStyle Hidden
 }
 #----------------------------------------------------------------------------------------------
 
--- scp_atualizar_runtime.ps1
+++ scp_atualizar_runtime.ps1
@@ -51,47 +51,131 @@ Write-Host "║" -NoNewline -ForegroundColor White
 write-host ("═" * 120) -NoNewline -ForegroundColor Cyan
 Write-Host "║" -ForegroundColor Cyan
 
+# Visual C 2005
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
-Write-Host ("{0,-86} "   -f "Visual C 2005") -NoNewline -ForegroundColor Green
+Write-Host ("{0,-86} " -f "Visual C 2005") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"vcredist2005_x64.exe" -ArgumentList "/q" -Wait
 
-Write-Host "║" -NoNewline -ForegroundColor Cyan
-Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
-Write-Host ("{0,-86} "   -f "Visual C 2008") -NoNewline -ForegroundColor Green
-Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"vcredist2008_x64.exe" -ArgumentList "/q" -Wait
+$vcredist2005Path = Join-Path $updatesDirectory "vcredist2005_x64.exe"
 
-Write-Host "║" -NoNewline -ForegroundColor Cyan
-Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
-Write-Host ("{0,-86} "   -f "Visual C 2010") -NoNewline -ForegroundColor Green
-Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"vcredist2010_x64.exe" -ArgumentList "/q" -Wait
+if (Test-Path $vcredist2005Path) {
+    Start-Process -FilePath $vcredist2005Path -ArgumentList "/q" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "Visual C 2005 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
 
+# Visual C 2008
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
-Write-Host ("{0,-86} "   -f "Visual C 2012") -NoNewline -ForegroundColor Green
+Write-Host ("{0,-86} " -f "Visual C 2008") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"vcredist2012_x64.exe" -ArgumentList "/q" -Wait
 
-Write-Host "║" -NoNewline -ForegroundColor Cyan
-Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
-Write-Host ("{0,-86} "   -f "Visual C 2013") -NoNewline -ForegroundColor Green
-Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"vcredist2013_x64.exe" -ArgumentList "/q" -Wait
+$vcredist2008Path = Join-Path $updatesDirectory "vcredist2008_x64.exe"
 
-Write-Host "║" -NoNewline -ForegroundColor Cyan
-Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
-Write-Host ("{0,-86} "   -f "Visual C 2015 / 2017 / 2019 / 2022") -NoNewline -ForegroundColor Green
-Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"vcredist2015_2017_2019_2022_x64.exe" -ArgumentList "/q" -Wait
+if (Test-Path $vcredist2008Path) {
+    Start-Process -FilePath $vcredist2008Path -ArgumentList "/q" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "Visual C 2008 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
 
+# Visual C 2010
+#----------------------------------------------------------------------------------------------
 Write-Host "║" -NoNewline -ForegroundColor Cyan
 Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
-Write-Host ("{0,-86} "   -f "Visual C Universal") -NoNewline -ForegroundColor Green
+Write-Host ("{0,-86} " -f "Visual C 2010") -NoNewline -ForegroundColor Green
 Write-Host "║" -ForegroundColor Cyan
-Start-Process -FilePath $updatesDirectory\"Windows8.1-KB2999226-x64.msu" -ArgumentList "/quiet" -Wait
+
+$vcredist2010Path = Join-Path $updatesDirectory "vcredist2010_x64.exe"
+
+if (Test-Path $vcredist2010Path) {
+    Start-Process -FilePath $vcredist2010Path -ArgumentList "/q" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "Visual C 2010 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Visual C 2012
+#----------------------------------------------------------------------------------------------
+Write-Host "║" -NoNewline -ForegroundColor Cyan
+Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
+Write-Host ("{0,-86} " -f "Visual C 2012") -NoNewline -ForegroundColor Green
+Write-Host "║" -ForegroundColor Cyan
+
+$vcredist2012Path = Join-Path $updatesDirectory "vcredist2012_x64.exe"
+
+if (Test-Path $vcredist2012Path) {
+    Start-Process -FilePath $vcredist2012Path -ArgumentList "/q" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "Visual C 2012 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Visual C 2013
+#----------------------------------------------------------------------------------------------
+Write-Host "║" -NoNewline -ForegroundColor Cyan
+Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
+Write-Host ("{0,-86} " -f "Visual C 2013") -NoNewline -ForegroundColor Green
+Write-Host "║" -ForegroundColor Cyan
+
+$vcredist2013Path = Join-Path $updatesDirectory "vcredist2013_x64.exe"
+
+if (Test-Path $vcredist2013Path) {
+    Start-Process -FilePath $vcredist2013Path -ArgumentList "/q" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "Visual C 2013 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Visual C 2015 / 2017 / 2019 / 2022
+#----------------------------------------------------------------------------------------------
+Write-Host "║" -NoNewline -ForegroundColor Cyan
+Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
+Write-Host ("{0,-86} " -f "Visual C 2015 / 2017 / 2019 / 2022") -NoNewline -ForegroundColor Green
+Write-Host "║" -ForegroundColor Cyan
+
+$vcredist2015_2022Path = Join-Path $updatesDirectory "vcredist2015_2017_2019_2022_x64.exe"
+
+if (Test-Path $vcredist2015_2022Path) {
+    Start-Process -FilePath $vcredist2015_2022Path -ArgumentList "/q" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "Visual C 2015 / 2017 / 2019 / 2022 não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
+
+# Visual C Universal
+#----------------------------------------------------------------------------------------------
+Write-Host "║" -NoNewline -ForegroundColor Cyan
+Write-Host ("{0,-30} : " -f " Instalando") -NoNewline -ForegroundColor White
+Write-Host ("{0,-86} " -f "Visual C Universal") -NoNewline -ForegroundColor Green
+Write-Host "║" -ForegroundColor Cyan
+
+$visualCUniversalPath = Join-Path $updatesDirectory "Windows8.1-KB2999226-x64.msu"
+
+if (Test-Path $visualCUniversalPath) {
+    Start-Process -FilePath $visualCUniversalPath -ArgumentList "/quiet" -Wait
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f " Aviso") -NoNewline -ForegroundColor Yellow
+    Write-Host ("{0,-86} " -f "Visual C Universal não encontrado no diretório.") -NoNewline -ForegroundColor Red
+    Write-Host "║" -ForegroundColor Cyan
+}
 #----------------------------------------------------------------------------------------------
 
 # Aplicando alterações
