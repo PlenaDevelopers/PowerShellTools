aec8fc089d41a76d6d9d02e322dbf8f7f844fe93
Plena Soluções <ecampanha@hotmail.com>
2024-08-22 18:27:09 -0300

    22-Agosto-2024

---
+++ outros/scp_oem_windows_serial.ps1
@@ -0,0 +1,20 @@
+﻿# Script para recuperar o Serial OEM do Windows na BIOS
+#-------------------------------------------------------
+$osVersion = (Get-WmiObject -Class Win32_OperatingSystem).Caption
+
+Write-Host "Sistema Operacional: $osVersion" -ForegroundColor Cyan
+Write-Host "Buscando o Serial OEM do Windows na BIOS..." -ForegroundColor Yellow
+
+try {
+    $oemKey = (Get-WmiObject -Query 'SELECT * FROM SoftwareLicensingService').OA3xOriginalProductKey
+
+    if ($oemKey) {
+        Write-Host "O Serial OEM encontrado na BIOS é:" -ForegroundColor Green
+        Write-Host $oemKey -ForegroundColor White
+    } else {
+        Write-Host "Nenhum Serial OEM foi encontrado na BIOS." -ForegroundColor Red
+    }
+} catch {
+    Write-Host "Erro ao tentar recuperar o Serial OEM." -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Red
+}
