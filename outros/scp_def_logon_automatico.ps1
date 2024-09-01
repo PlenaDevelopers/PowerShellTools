452cd777ed324d4447721a7562ead41b18c24c26
Plena Soluções <ecampanha@hotmail.com>
2024-09-01 17:21:30 -0300

    01-Setemebro-2024

--- outros/scp_def_logon_automatico.ps1
+++ outros/scp_def_logon_automatico.ps1
@@ -1,32 +1,32 @@
 ﻿<#
     Função: Habilitar/Desabilitar o Logon Automático
-	Copyright: © Plena Soluções - 2024
-	Date: Agosto/2024
+    Copyright: © Plena Soluções - 2024
+    Date: Agosto/2024
 
-	Licenciamento:
-	Este script é fornecido "como está", sem qualquer garantia de qualquer tipo,
-	expressa ou implícita, incluindo, mas não se limitando às garantias de 
-	comercialização, adequação a um determinado fim e não violação. O uso deste 
-	script é totalmente gratuito, mas você deve manter os créditos ao autor original.
-	
-	Seriais/Keys:
-	Os Seriais/Keys para licenciamento de software contidos neste ou em outros
-	arquivos são meramente ilustrativos para a utilização do script, sendo assim cabe
-	ao utilizador do script alterar estas chaves para uma válida que represente o 
-	licenciamento vigente.
+    Licenciamento:
+    Este script é fornecido "como está", sem qualquer garantia de qualquer tipo,
+    expressa ou implícita, incluindo, mas não se limitando às garantias de 
+    comercialização, adequação a um determinado fim e não violação. O uso deste 
+    script é totalmente gratuito, mas você deve manter os créditos ao autor original.
+    
+    Seriais/Keys:
+    Os Seriais/Keys para licenciamento de software contidos neste ou em outros
+    arquivos são meramente ilustrativos para a utilização do script, sendo assim cabe
+    ao utilizador do script alterar estas chaves para uma válida que represente o 
+    licenciamento vigente.
 
-	Bugs & Correções
-	Em caso de Bugs encontrado pedimos a gentileza de informar por email para que possamos 
-	analizar e gerar atualizações corretivas.
+    Bugs & Correções:
+    Em caso de Bugs encontrado pedimos a gentileza de informar por email para que possamos 
+    analisar e gerar atualizações corretivas.
 
-	Autor: Evandro Campanhã
-	Contato: aurora.erp@gmail.com
-	------------------------------------------------------------------------------
+    Autor: Evandro Campanhã
+    Contato: aurora.erp@gmail.com
+    ------------------------------------------------------------------------------
 #>
 param (
     [string]$Usuario = 'Administrador',
     [string]$Senha = 'P@ssw0rd',
-    [string]$HabilitarLogonAutomatico = '0'  # '1' para Habilitar, '0' para Desabilitar
+    [string]$HabilitarLogonAutomatico = '1'  # '1' para Habilitar, '0' para Desabilitar
 )
 
 # Cabeçalho
@@ -46,51 +46,100 @@ $cabecalhoScriptPath = Join-Path -Path $scriptDirectory -ChildPath "scp_script_c
 
 # Iniciar Ações
 #----------------------------------------------------------------------------------------------
-if ($HabilitarLogonAutomatico -eq "0") {
-    $acao = "Desabilitar"
-} elseif ($HabilitarLogonAutomatico -eq "1") {
-    $acao = "Habilitar"
+# Valor a ser configurado (1 para ativar e 0 para desativar)
+if ($HabilitarLogonAutomatico -eq "1") {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f "Opção") -NoNewline
+    Write-Host ("{0,-86} " -f "Ativar") -NoNewline -ForegroundColor White
+    Write-Host "║" -ForegroundColor Cyan
+} else {
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f "Opção") -NoNewline
+    Write-Host ("{0,-86} " -f "Desativar") -NoNewline -ForegroundColor White
+    Write-Host "║" -ForegroundColor Cyan
 }
+
+# Converter o nome do parametro
+$regValue = $HabilitarLogonAutomatico
+
+$regPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+$regName = "AutoAdminLogon"
+
 Write-Host "║" -NoNewline -ForegroundColor Cyan
-Write-Host ("{0,-30} : " -f "Valor") -NoNewline
-Write-Host ("{0,-86} " -f ("AutoAdminLogon: $HabilitarLogonAutomatico")) -NoNewline -ForegroundColor White
+Write-Host ("{0,-30} : " -f "Chave") -NoNewline
+Write-Host ("{0,-86} " -f $regPath) -NoNewline -ForegroundColor White
 Write-Host "║" -ForegroundColor Cyan
 
-$RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+Write-Host "║" -NoNewline -ForegroundColor Cyan
+Write-Host ("{0,-30} : " -f "Item") -NoNewline
+Write-Host ("{0,-86} " -f $regName) -NoNewline -ForegroundColor White
+Write-Host "║" -ForegroundColor Cyan
+
+Write-Host "║" -NoNewline -ForegroundColor Cyan
+Write-Host ("{0,-30} : " -f "Valor") -NoNewline
+Write-Host ("{0,-86} " -f $regValue) -NoNewline -ForegroundColor White
+Write-Host "║" -ForegroundColor Cyan
+
 # Verificar se o caminho no Registro existe, criar se não existir
-if (-not (Test-Path $RegistryPath)) {
-    $null=New-Item -Path $RegistryPath -Force | Out-Null
+if (-not (Test-Path $regPath)) {
+    $null = New-Item -Path $regPath -Force | Out-Null
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f "Chave") -NoNewline
+    Write-Host ("{0,-86} " -f "Criada") -NoNewline -ForegroundColor Green
+    Write-Host "║" -ForegroundColor Cyan
 }
 
 # Configurar o AutoAdminLogon
-$null=Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "$HabilitarLogonAutomatico" -Type String
-Write-Host "║" -NoNewline -ForegroundColor Cyan
-Write-Host ("{0,-30} : " -f "Chave") -NoNewline
-Write-Host ("{0,-86} " -f $RegistryPath) -NoNewline -ForegroundColor White
-Write-Host "║" -ForegroundColor Cyan
+$null = Set-ItemProperty -Path $regPath -Name $regName -Value $regValue -Type String
 
 # Configurar as chaves DefaultUsername e DefaultPassword apenas se o AutoAdminLogon estiver habilitado
 if ($HabilitarLogonAutomatico -eq '1') {
-    $null=Set-ItemProperty $RegistryPath 'DefaultUsername' -Value "$Usuario" -type String 
+    $null = Set-ItemProperty $RegPath 'DefaultUsername' -Value "$Usuario" -Type String
     Write-Host "║" -NoNewline -ForegroundColor Cyan
     Write-Host ("{0,-30} : " -f "Valor") -NoNewline
-    Write-Host ("{0,-86} " -f ("DefaultUsername: $Usuario")) -NoNewline -ForegroundColor White
+    Write-Host ("{0,-86} " -f ("DefaultUsername: $Usuario - Criado")) -NoNewline -ForegroundColor Green
     Write-Host "║" -ForegroundColor Cyan
 
-
-    $null=Set-ItemProperty $RegistryPath 'DefaultPassword' -Value "$Senha" -type String
+    $null = Set-ItemProperty $RegPath 'DefaultPassword' -Value "$Senha" -Type String
     Write-Host "║" -NoNewline -ForegroundColor Cyan
     Write-Host ("{0,-30} : " -f "Valor") -NoNewline
-    Write-Host ("{0,-86} " -f ("DefaultPassword: $Senha")) -NoNewline -ForegroundColor White
+    Write-Host ("{0,-86} " -f ("DefaultPassword: $Senha - Criado")) -NoNewline -ForegroundColor Green
     Write-Host "║" -ForegroundColor Cyan
 } else {
     # Remover as chaves DefaultUsername e DefaultPassword se o AutoAdminLogon estiver desabilitado
-    $null=Remove-ItemProperty $RegistryPath -Name 'DefaultUsername'
-    $null=Remove-ItemProperty $RegistryPath -Name 'DefaultPassword'
-    Write-Host "║" -NoNewline -ForegroundColor Cyan
-    Write-Host ("{0,-30} : " -f "Valor") -NoNewline
-    Write-Host ("{0,-86} " -f ("DefaultUsername e DefaultPassword removidos")) -NoNewline -ForegroundColor White
-    Write-Host "║" -ForegroundColor Cyan
+    if (Test-Path $regPath) {
+        $usernameExists = Get-ItemProperty -Path $regPath -Name 'DefaultUsername' -ErrorAction SilentlyContinue
+        if ($usernameExists) {
+            $usernameValue = Get-ItemPropertyValue -Path $regPath -Name 'DefaultUsername'
+            $null = Remove-ItemProperty $regPath -Name 'DefaultUsername'
+            Write-Host "║" -NoNewline -ForegroundColor Cyan
+            Write-Host ("{0,-30} : " -f "Valor") -NoNewline
+            Write-Host ("{0,-86} " -f ("DefaultUsername: $usernameValue Removido")) -NoNewline -ForegroundColor Green
+            Write-Host "║" -ForegroundColor Cyan
+        } else {
+            Write-Host "║" -NoNewline -ForegroundColor Cyan
+            Write-Host ("{0,-30} : " -f "Valor") -NoNewline
+            Write-Host ("{0,-86} " -f "DefaultUsername: Não havia um valor") -NoNewline -ForegroundColor Yellow
+            Write-Host "║" -ForegroundColor Cyan
+        }
+    }
+
+    if (Test-Path $regPath) {
+        $passwordExists = Get-ItemProperty -Path $regPath -Name 'DefaultPassword' -ErrorAction SilentlyContinue
+        if ($passwordExists) {
+            $passwordValue = Get-ItemPropertyValue -Path $regPath -Name 'DefaultPassword'
+            $null = Remove-ItemProperty $regPath -Name 'DefaultPassword'
+            Write-Host "║" -NoNewline -ForegroundColor Cyan
+            Write-Host ("{0,-30} : " -f "Valor") -NoNewline
+            Write-Host ("{0,-86} " -f ("DefaultPassword: $passwordValue Removido")) -NoNewline -ForegroundColor Green
+            Write-Host "║" -ForegroundColor Cyan
+        } else {
+            Write-Host "║" -NoNewline -ForegroundColor Cyan
+            Write-Host ("{0,-30} : " -f "Valor") -NoNewline
+            Write-Host ("{0,-86} " -f "DefaultPassword: Não havia um valor") -NoNewline -ForegroundColor Yellow
+            Write-Host "║" -ForegroundColor Cyan
+        }
+    }
 }
 #----------------------------------------------------------------------------------------------
 
--- outros/scp_desktop_bloquear_alteracoes.ps1
+++ outros/scp_desktop_bloquear_alteracoes.ps1
@@ -1,25 +1,53 @@
-﻿#Script para bloquear/desbloquear o desktop
+﻿# Script para bloquear/desbloquear configurações de personalização
 param (
-    [string]$acao = "0" # "0" para desbloquear, "1" para bloquear
+    [string]$acao = "1" # "0" para desbloquear, "1" para bloquear
 )
 
-# Defina o caminho do Registro e os valores
-$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop"
-$regName = "NoChangingWallPaper"
-
-if (-not (Test-Path $regPath)) {
-    New-Item -Path $regPath -Force | Out-Null
+# Função para definir valores no Registro
+function Set-RegistryValue {
+    param (
+        [string]$regPath,
+        [string]$regName,
+        [int]$regValue
+    )
+    
+    if (-not (Test-Path $regPath)) {
+        New-Item -Path $regPath -Force | Out-Null
+    }
+    
+    Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
 }
 
-# Valor a ser configurado (1 para ativar e 0 para desativar)
+# Bloqueio ou desbloqueio de alteração do papel de parede
+Set-RegistryValue -regPath "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" -regName "NoChangingWallPaper" -regValue $acao
+
+# Bloqueio ou desbloqueio de alteração de cores de destaque
+$regPathAccentColor = "HKCU:\Software\Policies\Microsoft\Windows\Personalization"
+$regNameAccentColor = "NoChangingAccentColor"
+
 if ($acao -eq "1") {
-    $regValue = 1 # Bloquear
+    # Bloqueio: Define a chave que impede a alteração de cores de destaque
+    Set-RegistryValue -regPath $regPathAccentColor -regName $regNameAccentColor -regValue 1
+    Write-Host "║" -NoNewline -ForegroundColor Cyan
+    Write-Host ("{0,-30} : " -f "Alteração de Cores de Destaque") -NoNewline
+    Write-Host ("{0,-86} " -f "Bloqueada") -NoNewline -ForegroundColor Green
+    Write-Host "║" -ForegroundColor Cyan
 } else {
-    $regValue = 0 # Desbloquear
+    # Desbloqueio: Remove a chave que permite alteração de cores de destaque
+    if (Test-Path $regPathAccentColor) {
+        Remove-ItemProperty -Path $regPathAccentColor -Name $regNameAccentColor -ErrorAction SilentlyContinue
+        Write-Host "║" -NoNewline -ForegroundColor Cyan
+        Write-Host ("{0,-30} : " -f "Alteração de Cores de Destaque") -NoNewline
+        Write-Host ("{0,-86} " -f "Desbloqueada") -NoNewline -ForegroundColor Green
+        Write-Host "║" -ForegroundColor Cyan
+    }
 }
 
-# Define o valor no Registro
-Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
+# Bloqueio ou desbloqueio de alteração da tela de fundo da tela de bloqueio
+Set-RegistryValue -regPath "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -regName "NoChangingLockScreen" -regValue $acao
+
+# Bloqueio ou desbloqueio de alteração de fontes
+Set-RegistryValue -regPath "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -regName "NoChangeFont" -regValue $acao
 
 # Verifica se o processo do Windows Explorer está em execução
 $explorerProcess = Get-Process -Name explorer -ErrorAction SilentlyContinue
