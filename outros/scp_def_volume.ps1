df7d560f40b1fd011b9add4d13484860981d770b
Plena Soluções <ecampanha@hotmail.com>
2024-09-02 20:58:37 -0300

    02-Setembro-2024

---
+++ outros/scp_def_volume.ps1
@@ -0,0 +1,152 @@
+﻿<#
+    Função: Ajustar Volume do Sistema
+    Copyright: © Plena Soluções - 2024
+    Date: Setembro/2024
+
+    Licenciamento:
+    Este script é fornecido "como está", sem qualquer garantia de qualquer tipo,
+    expressa ou implícita, incluindo, mas não se limitando às garantias de 
+    comercialização, adequação a um determinado fim e não violação. O uso deste 
+    script é totalmente gratuito, mas você deve manter os créditos ao autor original.
+    
+    Seriais/Keys:
+    Os Seriais/Keys para licenciamento de software contidos neste ou em outros
+    arquivos são meramente ilustrativos para a utilização do script, sendo assim cabe
+    ao utilizador do script alterar estas chaves para uma válida que represente o 
+    licenciamento vigente.
+
+    Bugs & Correções
+    Em caso de Bugs encontrado pedimos a gentileza de informar por email para que possamos 
+    analisar e gerar atualizações corretivas.
+
+    Autor: Evandro Campanhã
+    Contato: aurora.erp@gmail.com
+    ------------------------------------------------------------------------------
+#>
+
+param(
+    [float]$Volume = 50
+)
+
+# Cabeçalho
+#----------------------------------------------------------------------------------------------
+# Obter o diretório do script atual
+$scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+
+# Obter o nome do script atual
+$scriptName = [System.IO.Path]::GetFileName($MyInvocation.MyCommand.Path)
+
+# Construir o caminho completo para o script 'scp_script_cabecalho.ps1'
+$cabecalhoScriptPath = Join-Path -Path $scriptDirectory -ChildPath "scp_script_cabecalho.ps1"
+
+# Executar o script de cabeçalho
+& $cabecalhoScriptPath -Script $scriptName -Titulo "Ajustar Volume do Sistema"
+#----------------------------------------------------------------------------------------------
+
+# Iniciar Ações
+#----------------------------------------------------------------------------------------------
+# Função para adicionar o tipo de áudio
+function Add-AudioType {
+    if (-not [Audio]) {
+        Add-Type -TypeDefinition @'
+using System;
+using System.Runtime.InteropServices;
+
+[Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IAudioEndpointVolume {
+    int f(); // Métodos não utilizados
+    int g();
+    int h();
+    int i();
+    int SetMasterVolumeLevelScalar(float fLevel, Guid pguidEventContext);
+    int j();
+    int GetMasterVolumeLevelScalar(out float pfLevel);
+    int k();
+    int l();
+    int m();
+    int n();
+    int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, Guid pguidEventContext);
+    int GetMute(out bool pbMute);
+}
+
+[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDevice {
+    int Activate(ref Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+}
+
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDeviceEnumerator {
+    int f(); // Não utilizado
+    int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+}
+
+[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] 
+class MMDeviceEnumeratorComObject { }
+
+public class Audio {
+    static IAudioEndpointVolume Vol() {
+        var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+        IMMDevice dev = null;
+        Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(0, 1, out dev));
+        IAudioEndpointVolume epv = null;
+        var epvid = typeof(IAudioEndpointVolume).GUID;
+        Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, 23, 0, out epv));
+        return epv;
+    }
+
+    public static float Volume {
+        get { float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, Guid.Empty)); }
+    }
+
+    public static bool Mute {
+        get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, Guid.Empty)); }
+    }
+}
+'@
+    }
+}
+
+# Função para ajustar o volume e desmutar o áudio
+function Adjust-Audio {
+    param (
+        [float]$VolumeLevel = 0.9,  # Volume padrão a ser ajustado (90%)
+        [bool]$Mute = $false        # Desmutar o áudio
+    )
+
+    # Adiciona o tipo se ainda não estiver carregado
+    Add-AudioType
+
+    # Ajusta o volume e o estado de mudo
+    [Audio]::Volume = $VolumeLevel
+    [Audio]::Mute = $Mute
+}
+
+Write-Host "║" -NoNewline -ForegroundColor Cyan
+Write-Host ("{0,-30} : " -f "Volume") -NoNewline
+Write-Host ("{0,-86} " -f $Volume) -NoNewline -ForegroundColor Green
+Write-Host "║" -ForegroundColor Cyan
+
+# Divide o valor da variável por 100
+$volume = $Volume / 100
+
+Adjust-Audio -VolumeLevel $volume -Mute $false
+#----------------------------------------------------------------------------------------------
+
+# Aplicando alterações
+#----------------------------------------------------------------------------------------------
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+#----------------------------------------------------------------------------------------------
+
+# Rodapé
+#----------------------------------------------------------------------------------------------
+# Obter o diretório do script atual
+$CurrentScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+
+# Construir o caminho completo para o script 'scp_script_rodape.ps1'
+$rodapeScriptPath = Join-Path -Path $CurrentScriptDirectory -ChildPath "scp_script_rodape.ps1"
+
+# Executar o script de rodapé
+& $rodapeScriptPath
+#----------------------------------------------------------------------------------------------
