521dd4465d2f6d17d31a48ce16b467268444598d
Plena Soluções <ecampanha@hotmail.com>
2024-09-24 18:58:51 -0300

    24-Setembro-2024

---
+++ outros/scp_reinicia_bios.ps1
@@ -0,0 +1,11 @@
+﻿# Script PowerShell para reiniciar o sistema e entrar no BIOS/UEFI
+#-------------------------------------------------------------
+
+Write-Host "Reiniciando o sistema e entrando na configuração de BIOS/UEFI..." -ForegroundColor Cyan
+
+# Executa o comando de shutdown com parâmetros para reiniciar e entrar no firmware
+shutdown /r /fw /t 1
+
+Write-Host "O sistema será reiniciado em 1 segundo..." -ForegroundColor Green
+
+#-------------------------------------------------------------
